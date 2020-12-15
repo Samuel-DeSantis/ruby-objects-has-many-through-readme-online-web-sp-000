fd023a9add8f7bf9c422917dc245a9f3cc3cb5f1
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-15 04:50:26 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,15 @@
 class Customer
+  attr_accessor :name, :age
+
+  @@all = []
   
+  def initialize(name, age)
+    @name = name
+    @age = age
+    save
+  end
+
+  def save
+    @@all << self
+  end
 end
